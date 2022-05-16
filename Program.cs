02e271e862c42208c6b72dfad64777455891366d
Hernan Quiroga <quiroga.hernan@gmail.com>
2022-05-15 23:52:06 -0300

    Cambio de frases y agregué numero de intento al final

--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@
 int numAzar = new
     Random(DateTime.Now.Millisecond).Next(1, 21);
 
-string mensajeMenor = "El numero que elejiste es menor al que pensé.";
-string mensajeMayor = "El número que elegiste es mayor al que pensé.";
+string mensajeMenor = "Frio frio, piensa en grande.";
+string mensajeMayor = "No, no es tan grande el número que pensé.";
 
-Console.WriteLine("He pensado un número del 1 al 20 y quiero que lo adivines");
-Console.WriteLine("Ya sabes cual es ?");
+Console.WriteLine("He pensado un número del 1 al 20 y quiero que lo adivines.");
+Console.WriteLine("Qué número crees que he pensado ?");
 numUsuario = Convert.ToInt32(Console.ReadLine());
 
 
@@ -16,6 +16,7 @@ for (int i = 0; i < 21; i++)
     if (numUsuario == numAzar)
     {
         Console.WriteLine("Felicitaciones !! adivinaste el número era : " + numAzar);
+        Console.WriteLine("Lo adivinaste en sólo " + i + " intentos !!");
         break;
     }
     else if (numUsuario < numAzar)
